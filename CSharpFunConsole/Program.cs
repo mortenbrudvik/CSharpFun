a8c7742e247f8ff505123f271e82b8a665fe5734
Morten Brudvik <morten@brudvik.com>
2021-05-01 15:56:55 +0200

    is expression

--- CSharpFunConsole/Program.cs
+++ CSharpFunConsole/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using static System.Console;
+
+namespace CSharpFunConsole
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            WriteLine("Pattern matching C# 7");
+            
+            WriteLine("is expression");
+            const string colorDescription = "blue";
+            if (colorDescription is var newColorDescription)
+            {
+                newColorDescription += " and red";
+                WriteLine(newColorDescription);
+            }
+            
+            
+            
+
+
+            WriteLine("Hello World!");
+        }
+    }
+}
+    
+    

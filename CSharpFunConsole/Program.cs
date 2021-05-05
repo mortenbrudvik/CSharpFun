6f185af67f4aee88768fc7dcc7b847dbbad39e2e
Morten Brudvik <morten@brudvik.com>
2021-05-05 07:17:25 +0200

    Implemented KeyedCollection example

--- CSharpFunConsole/Program.cs
+++ CSharpFunConsole/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Data.Common;
+using System.Linq;
 using static System.Console;
 
 namespace CSharpFunConsole
@@ -10,7 +12,29 @@ namespace CSharpFunConsole
         private static void Main(string[] args)
         {
             CSharp7_Examples.Run();
+            
+            WriteLine("Collection of fun");
+            
+            WriteLine("KeyedCollection Example");
+            var persons = new PersonCollection {new Person("1", "Bob")};
+            foreach (var person in persons)
+            {
+                WriteLine(person);
+            }
+            
+
         }
+
+        public class PersonCollection : KeyedCollection<string, Person>
+        {
+            protected override string GetKeyForItem(Person person)
+            {
+                return person.Id;
+            }
+        }
+
+        internal record Person (string Id, string Name);
+
     }
 }
     
